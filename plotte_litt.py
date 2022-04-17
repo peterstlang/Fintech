508ee724c5e02cd3fbd38b0bd450af857824c12c
peterstlang <peter.chriss@hotmail.com>
2022-04-17 21:06:41 +0200

    tweaks

--- plotte_litt.py
+++ plotte_litt.py
@@ -13,9 +13,9 @@ import matplotlib.pyplot as plt
 
 import yhf_scraper as yhf
 
-url = yhf.get_stock('AAPL')
-#soup = yhf.gimme_soup(url)
-soup_w = yhf.gimme_soup_w_start_end(url, start='01-06-2021', end='01-01-2022')  
+url = yhf.get_stock("AAPL")
+# soup = yhf.gimme_soup(url)
+soup_w = yhf.gimme_soup_w_start_end(url, start="01-06-2021", end="01-01-2022")
 rows = yhf.soup_to_scrape(soup_w)
 scraped = yhf.scrape_it(rows)
 aapl_df = yhf.dicts_to_df(scraped)
@@ -28,24 +28,23 @@ cols = cols[1:7]
 # converts to num
 for i in cols:
     aapl_df[i] = pd.to_numeric(aapl_df[i])
-    
+
 # plot closing price
 # reversing
 
 aapl_df = aapl_df[::-1]
 
 
-aapl_df.plot(x='Date', y='Close', figsize=(10,6))
-plt.xticks(rotation = 45, ha = 'right')
+aapl_df.plot(x="Date", y="Close", figsize=(10, 6))
+plt.xticks(rotation=45, ha="right")
 
-#plot SMA
+# plot SMA
 def plot_SMA(df=aapl_df, time_period=10):
-    SMA = str('SMA' + '_' + str(time_period))
+    SMA = str("SMA" + "_" + str(time_period))
     df[SMA] = df.Close.rolling(int(time_period)).mean()
-    df.plot(x = 'Date', y = ['Close', SMA], figsize = (8,8))
-    plt.xticks(rotation = 45, ha = 'right')
+    df.plot(x="Date", y=["Close", SMA], figsize=(8, 8))
+    plt.xticks(rotation=45, ha="right")
+    plt.show()
+
 
 plot_SMA()
-
-
-
--- yhf_scraper.py
+++ yhf_scraper.py
@@ -4,13 +4,15 @@ Created on Mon Jan 10 21:42:42 2022
 
 @author: peter
 """
-#Checking package
+# Checking package
 
-#imports
+# imports
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
-#from selenium.webdriver.support.ui import select
+
+# from selenium.webdriver.support.ui import select
+
 
 def get_stock(symbol):
     "allows me to easily get any stock I want (from yahoo finance)"
@@ -18,135 +20,140 @@ def get_stock(symbol):
     return "https://finance.yahoo.com/quote/" + symbol + "/history?p=" + symbol
 
 
-def gimme_soup(url):
-    
-    driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    driver.get(url)
-    driver.find_element_by_xpath('//*[@value="agree"]').click()
-    time.sleep(10)
-    
-    
-    html = driver.page_source
-    soup = BeautifulSoup(html,'html5lib')
-    
-    return soup, driver
+# def gimme_soup(url):
 
-def gimme_soup_w_start_end(url, start='01-01-2018', end='01-01-2020'):
+#     driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+#     driver.maximize_window()
+#     driver.implicitly_wait(10)
+#     driver.get(url)
+#     driver.find_element_by_xpath('//*[@value="agree"]').click()
+#     time.sleep(10)
+
+#     html = driver.page_source
+#     soup = BeautifulSoup(html, "html5lib")
+
+#     return soup, driver
+
+
+def gimme_soup_w_start_end(url, start="01-01-2018", end="01-01-2020"):
     from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.common.by import By
-    from selenium.common.exceptions import TimeoutException
-    
+
+    # from selenium.webdriver.support.ui import WebDriverWait
+    # from selenium.webdriver.support import expected_conditions as EC
+    # from selenium.webdriver.common.by import By
+    # from selenium.common.exceptions import TimeoutException
+
     driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
     driver.maximize_window()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(1)
     driver.get(url)
     driver.find_element_by_xpath('//*[@value="agree"]').click()
+
     time.sleep(1)
-    
-    time.sleep(1)
-    drp = driver.find_element_by_xpath('//div[@class="Pos(r) D(ib) C($linkColor) Cur(p)"]/*[name()="svg"][@data-icon="CoreArrowDown"]')
+    drp = driver.find_element_by_xpath(
+        '//div[@class="Pos(r) D(ib) C($linkColor) Cur(p)"]/*[name()="svg"][@data-icon="CoreArrowDown"]'
+    )
     drp.click()
     time.sleep(1)
-    
-    name_start=driver.find_element_by_name("startDate")
+
+    name_start = driver.find_element_by_name("startDate")
     name_start.clear()
     name_start.send_keys(start)
-    
-    name_end=driver.find_element_by_name("endDate")
+
+    name_end = driver.find_element_by_name("endDate")
     name_end.clear()
     name_end.send_keys(end)
 
-    time.sleep(1)
+    time.sleep(0.5)
     driver.find_element_by_xpath('//span[contains(text(), "Done")]').click()
-    time.sleep(1)
+    time.sleep(0.5)
     driver.find_element_by_xpath('//span[contains(text(), "Apply")]').click()
-    time.sleep(3)
-    
+    time.sleep(1)
+
     last_height = driver.execute_script("return document.documentElement.scrollHeight")
-    
+
     while True:
-        driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
-        time.sleep(2)
-        new_height = driver.execute_script("return document.documentElement.scrollHeight")
-        
+        driver.find_element_by_xpath("//body").send_keys(Keys.CONTROL + Keys.END)
+        time.sleep(0.5)
+        new_height = driver.execute_script(
+            "return document.documentElement.scrollHeight"
+        )
+
         if new_height == last_height:
             break
         last_height = new_height
-        
-    #height = driver.execute_script("return document.documentElement.scrollHeight")
-    #print(height)
-    #driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
-    #time.sleep(2)
-    #height = driver.execute_script("return document.documentElement.scrollHeight")
-    #print(height)
-    #driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
-    #time.sleep(2)
-    #driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
-    #time.sleep(2)
-    #driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
-    #time.sleep(2)
-    #driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
-    #time.sleep(2)
 
-    
-    
+    # height = driver.execute_script("return document.documentElement.scrollHeight")
+    # print(height)
+    # driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
+    # time.sleep(2)
+    # height = driver.execute_script("return document.documentElement.scrollHeight")
+    # print(height)
+    # driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
+    # time.sleep(2)
+    # driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
+    # time.sleep(2)
+    # driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
+    # time.sleep(2)
+    # driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
+    # time.sleep(2)
+
     html = driver.page_source
-    soup = BeautifulSoup(html,'html5lib')
-    
+    soup = BeautifulSoup(html, "html5lib")
+
     return soup
+
 
 def soup_to_scrape(soup):
 
-    table = soup.find('table', class_='W(100%) M(0)')
-    rows = table.find_all('tr', class_='BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)')
-    
+    table = soup.find("table", class_="W(100%) M(0)")
+    rows = table.find_all("tr", class_="BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)")
+
     return rows
 
 
 def scrape_it(rows):
     data_list = []
-    
+
     for i in range(0, len(rows)):
         try:
             RowDict = {}
-            Values = rows[i].find_all('td')
-            
+            Values = rows[i].find_all("td")
+
             if len(Values) == 7:
-                RowDict["Date"] = Values[0].find('span').text.replace(',', '')
-                RowDict["Open"] = Values[1].find('span').text.replace(',', '')
-                RowDict["High"] = Values[2].find('span').text.replace(',', '')
-                RowDict["Low"] = Values[3].find('span').text.replace(',', '')
-                RowDict["Close"] = Values[4].find('span').text.replace(',', '')
-                RowDict["Adj Close"] = Values[5].find('span').text.replace(',', '')
-                RowDict["Volume"] = Values[6].find('span').text.replace(',', '')
-                
+                RowDict["Date"] = Values[0].find("span").text.replace(",", "")
+                RowDict["Open"] = Values[1].find("span").text.replace(",", "")
+                RowDict["High"] = Values[2].find("span").text.replace(",", "")
+                RowDict["Low"] = Values[3].find("span").text.replace(",", "")
+                RowDict["Close"] = Values[4].find("span").text.replace(",", "")
+                RowDict["Adj Close"] = Values[5].find("span").text.replace(",", "")
+                RowDict["Volume"] = Values[6].find("span").text.replace(",", "")
+
                 data_list.append(RowDict)
-                
+
         except:
             print("Row Number: " + str(i))
         finally:
             i = i + 1
-    
+
     return data_list
-            
+
+
 def dicts_to_df(list_of_dicts):
     import pandas as pd
+
     df = pd.DataFrame(list_of_dicts)
     return df
 
-    
 
 if __name__ == "__main__":
-    url = get_stock('AAPL')
-    soup = gimme_soup_w_start_end(url)   
+    url = get_stock("AAPL")
+    soup = gimme_soup_w_start_end(url)
     rows = soup_to_scrape(soup)
     scraped = scrape_it(rows)
     df = dicts_to_df(scraped)
-#    
+    print(df)
+#
 #    time.sleep(1)
 #    drp = driver.find_element_by_xpath('//div[@class="Pos(r) D(ib) C($linkColor) Cur(p)"]/*[name()="svg"][@data-icon="CoreArrowDown"]')
 #    drp.click()
@@ -166,10 +173,3 @@ if __name__ == "__main__":
 #    driver.find_element_by_xpath('//span[contains(text(), "Apply")]').click()
 #    time.sleep(1)
 #    #driver.find_element_by_xpath('//button[@class=" Bd Bdc($linkColor) Bdrs(3px) Px(20px) Miw(100px) Whs(nw) Fz(s) Fw(500) D(ib) C($linkColor) Bdc($linkActiveColor):h C($linkActiveColor):h Cur(p) Td(n)  Py(8px) cancel Miw(80px)! Fl(end)"]').click()
-
-
-
-
-
-    
-
