be71da7726b15af37d21de09bf7ad7caac9ab760
peterstlang <peter.chriss@hotmail.com>
2022-06-07 15:50:42 +0200

    rydde

--- plotte_litt.py
+++ plotte_litt.py
@@ -13,32 +13,6 @@ import matplotlib.pyplot as plt
 
 import yhf_scraper as yhf
 
-# url = yhf.get_stock("AAPL")
-# # soup = yhf.gimme_soup(url)
-# soup_w = yhf.gimme_soup_w_start_end(url, start="01-06-2021", end="01-01-2022")
-# rows = yhf.soup_to_scrape(soup_w)
-# scraped = yhf.scrape_it(rows)
-# aapl_df = yhf.dicts_to_df(scraped)
-
-
-# ### plotting
-# cols = list(aapl_df.columns)
-# cols = cols[1:7]
-
-# # converts to num
-# for i in cols:
-#     aapl_df[i] = pd.to_numeric(aapl_df[i])
-
-# plot closing price
-# reversing
-
-# df = df[::-1]
-
-
-# aapl_df.plot(x="Date", y="Close", figsize=(10, 6))
-# plt.xticks(rotation=45, ha="right")
-
-
 def plotte_prep(df=None):
     cols = list(df.columns)
     cols = cols[1:7]
@@ -72,10 +46,6 @@ def plot_SMA(df=None, time_period=[10]):
     plt.xticks(rotation=45, ha="right")
     plt.show()
 
-
-# time_list = [5,10,25,50]
-# for i in range(len(time_list)):
-#     plot_SMA(time_period = time_list[i])
 
 if __name__ == "__main__":
     df = execute_scrape()
