bfd6ac9c45d101104a99895d63aba05480905a8a
peterstlang <peter.chriss@hotmail.com>
2022-06-25 21:52:58 +0200

    created file for backtesting

---
+++ backtest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 25 21:46:02 2022
+
+@author: peter
+"""
+import plotte_litt as plitt
+import yfinance as yf 
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    #df = plitt.execute_scrape()
+    #prepped = plitt.plotte_prep(df)
+    #plitt.plot_CMA(df=prepped, col="Close")
